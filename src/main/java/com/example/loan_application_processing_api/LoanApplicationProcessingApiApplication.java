a24c1c4d2a1b694c5b163d24acdb2ef3562cb762
nazartsentelevych <nazartsentelevych@gmail.com>
2022-10-03 13:48:17 +0300

    Initiate project

---
+++ src/main/java/com/example/loan_application_processing_api/LoanApplicationProcessingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.loan_application_processing_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationProcessingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplicationProcessingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loan_application_processing_api/LoanApplicationProcessingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loan_application_processing_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationProcessingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
